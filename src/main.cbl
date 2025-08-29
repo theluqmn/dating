4d97086d5470ec5d2e8d650f613604d6207ee969
theluqmn <luqman.yazrie@outlook.com>
2025-08-29 21:46:52 +0800

    restructured date system thingy

--- src/main.cbl
+++ src/main.cbl
@@ -8,17 +8,31 @@
        WORKING-STORAGE SECTION.
       *logic variables
        01 COUNTER                  PIC 9(4).
-       01 WS-CURRENT-DATE          PIC 9(8).
-       01 WS-CURRENT-DATE-REDEF REDEFINES WS-CURRENT-DATE.
-           05 WS-CURRENT-YEAR      PIC 9(4).
-           05 WS-CURRENT-MONTH     PIC 9(2).
-           05 WS-CURRENT-DAY       PIC 9(2).
+       01 CLI-INPUT                PIC X(32)
+       01 WS-DATE                  PIC 9(8).
+       01 WS-DATE-REDEF REDEFINES WS-DATE.
+           05 WS-DATE-YEAR         PIC 9(4).
+           05 WS-DATE-MONTH        PIC 9(2).
+           05 WS-DATE-DAY          PIC 9(2).
+      *temporary variables
+       01 TP-STR-A                 PIC X(32).
+       01 TP-DATE                  PIC 9(8).
+       01 TP-DATE-REDEF REDEFINES TP-DATE.
+           05 TP-DATE-YEAR         PIC 9(4).
+           05 TP-DATE-MONTH        PIC 9(2).
+           05 TP-DATE-DAY          PIC 9(2).
 
        PROCEDURE DIVISION.
-       ACCEPT WS-CURRENT-DATE FROM DATE YYYYMMDD.
+       ACCEPT WS-DATE FROM DATE YYYYMMDD.
        DISPLAY "DATING - date calculation tool".
-       DISPLAY WS-CURRENT-DATE.
-       DISPLAY WS-CURRENT-YEAR.
-       MOVE 20090416 TO WS-CURRENT-DATE.
-       DISPLAY WS-CURRENT-DATE.
+       DISPLAY "current date: " WS-DATE.
+       PERFORM PROCEDURE-MAIN.
+       CLI-HANDLER.
+           DISPLAY "---------------------------------------------".
+           DISPLAY "> " WITH NO ADVANCING.
+           ACCEPT TP-STR-A.
+           MOVE FUNCTION LOWER-CASE(TP-STR-A) TO CLI-INPUT.
+       PROCEDURE-MAIN.
+           PERFORM CLI-HANDLER UNTIL CLI-INPUT = "exit".
+           STOP RUN.
        END PROGRAM DATING.
